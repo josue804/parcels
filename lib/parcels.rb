67fefa8b72d79910a1f9b8ef8660c730fd0b9c93
Josue Valverde & Tim Kellogg <student@epicodus.com>
2015-08-10 12:28:38 -0700

    add code to return % of total price that employees pay.

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -16,14 +16,22 @@ class Parcel
 
   define_method(:wrap) do |choice|
     if choice
-    surface_area = (2 * (@length * @width) + 2 * (@width * @height) + 2 * (@length * @height))
-    surface_area * 0.01
+      surface_area = (2 * (@length * @width) + 2 * (@width * @height) + 2 * (@length * @height))
+      surface_area * 0.01
     else
       0
     end
   end
 
-  define_method(:total) do |miles, days, wrap_choice|
-    cost_to_ship(miles, days) + wrap(wrap_choice)
+  define_method(:total) do |miles, days, wrap_choice, is_employee|
+    (cost_to_ship(miles, days) + wrap(wrap_choice)) * discount?(is_employee)
+  end
+
+  define_method(:discount?) do |is_employee = false|
+    if is_employee
+      0.90
+    else
+      1
+    end
   end
 end
