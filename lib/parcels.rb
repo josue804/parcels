2b4f7b01cea2d49ba660909d14811fac6365d561
Josue Valverde & Tim Kellogg <student@epicodus.com>
2015-08-10 12:14:21 -0700

    add code to return the total cost of shipping and handling parcels.

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -14,8 +14,16 @@ class Parcel
     (miles * 0.2 + @volume * 0.2 - days * 200)/100
   end
 
-  define_method(:wrap) do
+  define_method(:wrap) do |choice|
+    if choice
     surface_area = (2 * (@length * @width) + 2 * (@width * @height) + 2 * (@length * @height))
     surface_area * 0.01
+    else
+      0
+    end
+  end
+
+  define_method(:total) do |miles, days, wrap_choice|
+    cost_to_ship(miles, days) + wrap(wrap_choice)
   end
 end
