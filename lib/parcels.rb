b53e64881a1481c4cd258105e72130d6ce1843a2
Josue Valverde & Tim Kellogg <student@epicodus.com>
2015-08-10 11:55:41 -0700

    add/pass method for calculating shipping cost

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,5 +1,16 @@
 class Parcel
   define_method(:initialize) do |length, width, height|
-    
+    @length = length
+    @width = width
+    @height = height
+    volume
+  end
+
+  define_method(:volume) do
+    @volume = @length * @width * @height
+  end
+
+  define_method(:cost_to_ship) do |miles, days|
+    (miles * 0.2 + @volume * 0.2 - days * 200)/100
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -2,10 +2,20 @@ require('rspec')
 require('parcels')
 
 describe(Parcel) do
+
   describe('#volume') do
     it('returns the volume') do
-      test_triangle = Parcel.new(10, 10, 10)
-      expect(test_triangle.volume()).to(eq(1000))
+      test_parcel = Parcel.new(10, 10, 10)
+      expect(test_parcel.volume()).to(eq(1000))
     end
   end
+
+  describe('#cost_to_ship') do
+    it('returns the expense of parcel shipment') do
+      test_parcel = Parcel.new(10, 10, 10)
+      expect(test_parcel.cost_to_ship(5000, 2)).to(eq(8))
+    end
+  end
+
+  
 end
